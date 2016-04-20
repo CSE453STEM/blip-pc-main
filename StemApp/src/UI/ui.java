0816f80c46a2db4ec0948343e7cede6530e7bb79
Kai Hsu <kaihsu94@gmail.com>
2016-04-20 01:16:58 -0400

    finished button bits that converts to asci

--- StemApp/src/UI/ui.java
+++ StemApp/src/UI/ui.java
@@ -1,8 +1,11 @@
 package UI;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+import javax.swing.JToggleButton;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.SimpleAttributeSet;
@@ -12,17 +15,22 @@ import javax.swing.text.PlainDocument;
 
 import transmit.transmitter;
 
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.List;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.util.LinkedList;
 
 public class ui extends JFrame{
 	private JTextPane chatbox = new JTextPane();
@@ -32,6 +40,19 @@ public class ui extends JFrame{
 	private JButton sendTypebox = new JButton("Send");
 	private JScrollPane scroll = new JScrollPane(chatbox);
 	private static transmitter tm;
+	
+	private JLabel displayLabel = new JLabel();
+	
+	private JCheckBox setBit1 = new JCheckBox("bit1");
+	private JCheckBox setBit2 = new JCheckBox("bit2");
+	private JCheckBox setBit3 = new JCheckBox("bit3");
+	private JCheckBox setBit4 = new JCheckBox("bit4");
+	private JCheckBox setBit5 = new JCheckBox("bit5");
+	private JCheckBox setBit6= new JCheckBox("bit6");
+	private JCheckBox setBit7= new JCheckBox("bit7");
+	private JButton sendBitBox = new JButton("Send");
+	private ButtonGroup bGroup = new ButtonGroup();
+	
 	
 	public static void main(String args[]) {
 		ui myUI = new ui();
@@ -54,6 +75,7 @@ public class ui extends JFrame{
 		
 		JPanel panel = new JPanel();
 		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
 		
 		Color myGreen = new Color (141,244,127);
 		Color myRed = new Color (160,4,4);
@@ -69,13 +91,38 @@ public class ui extends JFrame{
 		scroll.setPreferredSize(new Dimension(width/3, height/2));
 		typebox.setPreferredSize(new Dimension(width/3 - width/21, height/30));
 		sendTypebox.setPreferredSize(new Dimension(width/24, height/30));
+		sendBitBox.setPreferredSize(new Dimension(width/26, height/32));
+	//	panel3.setPreferredSize(new Dimension(width/2,height/30));
+		
+		displayLabel.setText("Check Bits to find Asci Character!");
+		
+		sendBitBox.setBackground(myRed); 
+		//bGroup.add(setBit1);
+		//bGroup.add(setBit2);
+		//bGroup.add(setBit3);
+		//bGroup.add(setBit4);
+		//bGroup.add(setBit5);
+		//bGroup.add(setBit6);
+		
 		
 		chatbox.setEditable(false);
 		panel.add(scroll);
 		panel2.add(typebox);
 		panel2.add(sendTypebox);
-		add(panel);
-		add(panel2, BorderLayout.SOUTH);
+		panel3.add(setBit7);
+		panel3.add(setBit6);
+		panel3.add(setBit5);
+		panel3.add(setBit4);
+		panel3.add(setBit3);
+		panel3.add(setBit2);
+		panel3.add(setBit1);
+		panel3.add(sendBitBox,BorderLayout.EAST);
+		panel3.add(Box.createHorizontalStrut(100));//creates space for ASCCI character 
+		panel3.add(displayLabel);
+		
+		add(panel,BorderLayout.NORTH);
+		add(panel2, BorderLayout.CENTER);
+		add(panel3, BorderLayout.SOUTH);
 		pack();
 		setVisible(true);
 		
@@ -86,6 +133,15 @@ public class ui extends JFrame{
 		/* Pass UI to data transmission handler */
 		tm = new transmitter(this);
 		MultiListener send = new MultiListener(tm);
+		MyActionListener but = new MyActionListener();
+		sendBitBox.addActionListener(but);
+		but.addCheckBox(setBit1);
+		but.addCheckBox(setBit2);
+		but.addCheckBox(setBit3);
+		but.addCheckBox(setBit4);
+		but.addCheckBox(setBit5);
+		but.addCheckBox(setBit6);
+		but.addCheckBox(setBit7);
 		typebox.addActionListener(send);
 		sendTypebox.addActionListener(send);
 		
@@ -118,6 +174,63 @@ public class ui extends JFrame{
 			}
 		}
 	}
+	
+	
+	public class MyActionListener implements ActionListener {
+		  private final java.util.List<JCheckBox> checkBoxes = new LinkedList<JCheckBox>();
+
+		  /**
+		   * Adds the specified JCheckBox to the list of JCheckBoxes.
+		   */
+		  public void addCheckBox(JCheckBox checkBox) {
+		    this.checkBoxes.add(checkBox);
+		  }
+
+		  /**
+		   * Called when the Submit button is pressed.
+		   */
+		  public void actionPerformed(ActionEvent evt) {
+		    StringBuilder sb = new StringBuilder();
+		   // sb.append("Selected Check Boxes: ");
+
+		    // Iterate over each JCheckBox and build message ready for display.
+		    // Could do something more sophisticated here if required.
+		    for (JCheckBox checkBox : checkBoxes) {
+		      
+		    	if (checkBox.isSelected()) {
+		        //sb.append(checkBox.getText().charAt(checkBox.getText().length()-1));
+		    	  sb.append("1");
+		      }
+		      else{
+		    	  sb.append("0");
+		      }
+		    }
+		    sb.reverse().toString();
+		    String ans = sb.toString();
+		    int asci = Integer.parseInt(ans, 2);
+		    String fin = Character.toString((char) asci);
+		   // JOptionPane.showMessageDialog(null, fin);
+		    displayLabel.setText(fin);
+		  }
+		}	
+	
+	
+	
+	
+	/*public class ButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent E) {
+			Object src = E.getSource();
+			
+			for(int i = 1; i<bGroup.getButtonCount();i++){
+				if(bGroup(i).)
+			}
+			
+		}
+		
+	}*/
+
 	
 	public void printReceived(String r) {
 		StyledDocument doc = chatbox.getStyledDocument();
